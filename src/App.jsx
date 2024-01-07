94a981b69a7a0f19e7f92ac89e6f8307a85f4a6f
Hemant Bisht <bishthemant169@gmail.com>
2024-01-08 01:00:54 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Success from './pages/Success'
 import Error from './pages/Error'
+import ProtectedRoute from './components/ProtectedRoute'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
-        <Route path='/success' element={<Success/>}></Route>
+        <Route path='/success' element={<ProtectedRoute element={<Success/>}/>}></Route>
         <Route path='/*' element={<Error/>}></Route>
       </Routes>
     </BrowserRouter>
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,6 +3,7 @@ import { IoMdClose } from "react-icons/io";
 import { FaShoppingCart } from "react-icons/fa";
 import { ItemCard } from "./ItemCard";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
   const [activeCart, setActiveCart] = useState(false);
@@ -11,6 +12,7 @@ const Cart = () => {
   const totalQty = cartItems.reduce((totalQty, item)=>totalQty+item.qty, 0)
   const totalPrice = cartItems.reduce((total, item)=>total+item.qty*item.price, 0)
 
+  const navigate=useNavigate()
   return (
     <>
       <div
@@ -43,7 +45,8 @@ const Cart = () => {
           <h3 className="font-semibold text-gray-800">Items : {totalQty}</h3>
           <h3 className="font-semibold text-gray-800">Total Amount : {totalPrice}</h3>
           <hr className="w-[90vw] lg:w-[18vw] my-2" />
-          <button className="bg-green-500 font-bold px-3 text-white py-2 rounded-lg w-[90vw] lg:w-[18vw] mb-5">
+          <button onClick={()=>navigate("/success")}
+          className="bg-green-500 font-bold px-3 text-white py-2 rounded-lg w-[90vw] lg:w-[18vw] mb-5">
             Checkout
           </button>
         </div>
